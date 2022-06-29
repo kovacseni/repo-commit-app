844e40537274839bfa29fa5e7f138069b8606bb8
kovacseni <kovacseni@gmail.com>
2022-06-29 13:55:43 +0200

    görgethető első sor és oszlop

--- src/main/java/commit/HtmlTable.java
+++ src/main/java/commit/HtmlTable.java
@@ -96,11 +96,7 @@ public class HtmlTable {
     private void addBeginTags() {
         htmlTable.add("""
                  <body>
-                     <div class="container py-3">
-                       <div class="row">
-                         <div class="my-3 mx-auto bg-white shadow rounded">
-                           <div class="table-responsive">
-                             <table class="table table-bordered" style="width: auto">
+                 <table class="table table-bordered sticky sticky-x" style="width: auto">
                 """);
     }
 
@@ -149,15 +145,14 @@ public class HtmlTable {
 
     private void addEndTags() {
         htmlTable.add("""
-               </table>
-                       </div>
-                           <script
-                             src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
-                             integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
-                             crossorigin="anonymous"
-                           ></script>
-                         </body>
-                       </html>
+                </table>
+                    <script
+                      src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+                      integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2"
+                      crossorigin="anonymous"
+                    ></script>
+                  </body>
+                </html>
                 """);
     }
 
